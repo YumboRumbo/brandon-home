9d05dabf045055c02a1607c356c844c1083a43df
Sang Yum <sangyum@gmail.com>
2018-04-17 23:06:33 -0700

    added bootstrap 4 support

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/index.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <div className="container-fluid">
+        <div class="jumbotron">
+          <h1 class="display-4">Hello, world!</h1>
+          <p class="lead">
+            This is a simple hero unit, a simple jumbotron-style component for
+            calling extra attention to featured content or information.
+          </p>
+          <hr class="my-4" />
+          <p>
+            It uses utility classes for typography and spacing to space content
+            out within the larger container.
+          </p>
+          <p class="lead">
+            <a class="btn btn-primary btn-lg" href="#" role="button">
+              Learn more
+            </a>
+          </p>
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
